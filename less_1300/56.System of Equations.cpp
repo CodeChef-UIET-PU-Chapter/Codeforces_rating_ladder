f885171cd02bbf0b39902b7c5c69ed2a86b9d3da
ANKAY <52236029+ankay212000@users.noreply.github.com>
2020-09-25 19:13:31 +0530

    Update 56.System of Equations.cpp

--- less_1300/56.System of Equations.cpp
+++ less_1300/56.System of Equations.cpp
@@ -1,31 +1,18 @@
 #include<bits/stdc++.h>
-#define ll long long
-#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); 
 using namespace std;
-#define N 100
- 
-bool isint(float a)
-{
-    if(ceil(a)-floor(a)==0)
-        return true;
-    else 
-    return false;
-}
  
 int main()
 {
-    io;
-    ll n,m;
-    cin>>n>>m;
-    ll arr[n];
-    for(ll i=0;i<n;i++) cin>>arr[i];
-    sort(arr,arr+n);
-    ll sum=0;
-    for(ll i=0;i<m;i++)
+    ll m,n,a=INT_MAX,b=0,count=0;
+    cin>>m>>n;
+    while(pow(b,2)<=m)
     {
-        if(arr[i]<0)
-        sum+=abs(arr[i]);
+        a=m-pow(b,2);
+        if(b==n-pow(a,2))
+        count++;
+        b++;
     }
-    cout<<sum;
-    return 0;
+    cout<<count;
 }

b08a4a680d391563593ff09bd8f70bb311af34d5
ANKAY <52236029+ankay212000@users.noreply.github.com>
2020-09-26 18:44:15 +0530

    Create 7.Levko and Table.cpp

---
+++ >=1300&&<=1399/7.Levko and Table.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+#define N 100000
+#define mod 1000000007
+ 
+template <typename T>
+void print(T x){cout<<x<<"\n";}
+template <typename T1, typename T2>
+void print2(T1 x,T2 y){cout<<x<<" "<<y<<"\n";}
+template <typename T1, typename T2,typename T3>
+void print3(T1 x, T2 y,T3 z){cout<<x<<" "<<y<<" "<<z<<"\n";}
+
+int main()
+{
+    io;
+    ll test_case;
+    //cin>>test_case;
+    test_case=1;
+    while(test_case--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int p=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j==n-p)
+                cout<<k<<" ",p++;
+                else
+                cout<<0<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+}

2145c2190b537cd6cdb7aac858240034750c7dad
ANKAY <52236029+ankay212000@users.noreply.github.com>
2020-09-26 19:25:28 +0530

    Create 8.Chip.cpp

---
+++ >=1300&&<=1399/8.Chip.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+#define N 100000
+#define mod 1000000007
+ 
+template <typename T>
+void print(T x){cout<<x<<"\n";}
+template <typename T1, typename T2>
+void print2(T1 x,T2 y){cout<<x<<" "<<y<<"\n";}
+template <typename T1, typename T2,typename T3>
+void print3(T1 x, T2 y,T3 z){cout<<x<<" "<<y<<" "<<z<<"\n";}
+
+int main()
+{
+    io;
+    ll test_case;
+    //cin>>test_case;
+    test_case=1;
+    while(test_case--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int res=n*(n+1)/2;
+        int left=k%res;
+        int poss=floor(-1+sqrt(1+8*(left)))/2;
+        //cout<<left<<" "<<poss<<"\n";
+        int terms=poss*(poss+1)/2;
+        cout<<left-terms<<"\n";
+    }
+}

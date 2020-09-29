0747cee73aacbc22eb0afe8d16ad70b4f8c31248
ANKAY <52236029+ankay212000@users.noreply.github.com>
2020-09-29 11:05:50 +0530

    Create 63.Little Elephant and Function.cpp

---
+++ less_1300/63.Little Elephant and Function.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+#define N 100
+ 
+ 
+int main()
+{
+    io;
+    ll n;
+    cin>>n;
+    cout<<n<<" ";
+    for(ll i=1;i<=n-1;i++) cout<<i<<" ";
+}

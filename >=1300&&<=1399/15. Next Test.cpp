26c516ebc5ac1fb730e64a3ec7ff0d15acbafa99
ANKAY <52236029+ankay212000@users.noreply.github.com>
2020-10-04 13:15:15 +0530

    Create 15. Next Test.cpp

---
+++ >=1300&&<=1399/15. Next Test.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+#define N 100000
+#define mod 1000000007
+ 
+template <typename T>
+void print(T x){cout<<x<<"\n";}
+template <typename T1, typename T2>
+void print2(T1 x,T2 y){cout<<x<<" "<<y<<"\n";}
+template <typename T1, typename T2,typename T3>
+void print3(T1 x, T2 y,T3 z){cout<<x<<" "<<y<<" "<<z<<"\n";}
+ 
+int main()
+{
+    io;
+    ll test_case;
+    //cin>>test_case;
+    test_case=1;
+    while(test_case--)
+    {
+        int n;
+        cin>>n;
+        vector<int> store(n+1);
+        for(int i=1;i<=n;i++)
+        cin>>store[i];
+
+        sort(store.begin(),store.end());
+        int i;
+        bool flag=false;
+        for(i=1;i<=store[n];i++)
+        {
+            if(store[i]!=i)
+            {
+              flag=true;
+              print(i);
+              break;
+            }  
+            //cout<<store[i]<<" "<<i<<"\n";
+        }
+        if(!flag)
+        print(i);
+    }  
+}
